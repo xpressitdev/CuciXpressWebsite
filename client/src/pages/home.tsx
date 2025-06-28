b6a77aa589b91c4864ec41458138dcb99d800427
hakemshahbirin <39381212-hakemshahbirin@users.noreply.replit.com>
2025-06-28 12:47:33 +0000

    Restored to '8d712ebe1965fa1b6ff9a929738b94271ef05d04'
    Replit-Restored-To: 8d712ebe1965fa1b6ff9a929738b94271ef05d04

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,10 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
-import WhatsNew from "@/components/WhatsNew";
-import NewPricing from "@/components/NewPricing";
-import Membership from "@/components/Membership";
-import Locations from "@/components/Locations";
+import Stats from "@/components/Stats";
 import Testimonials from "@/components/Testimonials";
+import Locations from "@/components/Locations";
+import Gallery from "@/components/Gallery";
+import PricingSection from "@/components/PricingSection";
 import Investment from "@/components/Investment";
 import Footer from "@/components/Footer";
 
@@ -14,11 +14,11 @@ export default function Home() {
       <Navigation />
       <main>
         <Hero />
-        <WhatsNew />
-        <NewPricing />
-        <Membership />
-        <Locations />
+        <Stats />
         <Testimonials />
+        <Locations />
+        <Gallery />
+        <PricingSection />
         <Investment />
       </main>
       <Footer />
